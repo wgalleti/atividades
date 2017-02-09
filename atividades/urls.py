4adb3124ec41e12d47841b9978e47c4d95ddc59e
William Galleti <william.galleti@gmail.com>
2017-02-09 02:46:36 -0300

    Adicionado TemplateView

--- atividades/urls.py
+++ atividades/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from atividades.core.views import *
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'',index),
+    url(r'^$',TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
     # jet
     url(r'^jet/', include('jet.urls', 'jet')),
